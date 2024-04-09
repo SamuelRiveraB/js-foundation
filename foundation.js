d86a7ea67aa492cf21683deb60dc9d4948637ef7
Samuel <samuelrivba@gmail.com>
2024-04-09 16:55:20 -0500

    Add interpreter vs compiler code

--- foundation.js
+++ foundation.js
@@ -3,3 +3,13 @@ function JSEngine(code) {
 }
 
 console.log(JSEngine("var a = 5"));
+
+// Interpreter vs compiler
+
+function someCalculation(x, y) {
+  return x + y;
+}
+
+for (let i = 0; i < 10; i++) {
+  console.log(someCalculation(5, 4));
+}
