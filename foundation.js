54cd6cb4be15413f13a564b0ecd4eafb20387b9a
Samuel <samuelrivba@gmail.com>
2024-04-09 22:19:54 -0500

    Add currying

--- foundation.js
+++ foundation.js
@@ -133,3 +133,14 @@ function getMaxNumber(arr) {
 }
 
 console.log(getMaxNumber(array));
+
+// Currying
+
+function multiply(a, b) {
+  return a * b;
+}
+
+let multiplyByTwo = multiply.bind(this, 2);
+console.log(multiplyByTwo(4));
+let multiplyByTen = multiply.bind(this, 10);
+console.log(multiplyByTen(4));
