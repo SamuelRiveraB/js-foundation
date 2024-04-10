8a6efe7772b94b957f2db9dbe2381522d56194f2
Samuel <samuelrivba@gmail.com>
2024-04-09 20:28:12 -0500

    Talk about function arguments

--- foundation.js
+++ foundation.js
@@ -71,3 +71,20 @@ var foodThoughts = function () {
 };
 
 foodThoughts();
+
+// Arguments
+
+function marry(person1, person2) {
+  console.log("arguments", arguments);
+  console.log(Array.from(arguments));
+  console.log(`${person1} is now married to ${person2}`);
+}
+
+function marry2(...args) {
+  console.log("arguments", args);
+  console.log(Array.from(arguments));
+  console.log(`${args[0]} is now married to ${args[1]}`);
+}
+
+marry("Tim", "Tina");
+marry2("Tim", "Tina");
