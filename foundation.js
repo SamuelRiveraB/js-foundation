d3941f0cf57a6d0d7f83ea04b62b85963b8d6670
Samuel <samuelrivba@gmail.com>
2024-04-09 19:20:26 -0500

    Add stack overflow exercise

--- foundation.js
+++ foundation.js
@@ -13,3 +13,17 @@ function someCalculation(x, y) {
 for (let i = 0; i < 10; i++) {
   console.log(someCalculation(5, 4));
 }
+
+// Stack overflow
+
+const list = new Array(60000).join("1.1").split(".");
+
+function removeItemsFromList() {
+  var item = list.pop();
+
+  if (item) {
+    removeItemsFromList();
+  }
+}
+
+removeItemsFromList();
