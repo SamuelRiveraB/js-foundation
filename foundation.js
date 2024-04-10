6ba47756a3fe5c9fa9f1da48f013eb673ce7eba5
Samuel <samuelrivba@gmail.com>
2024-04-09 22:02:41 -0500

    Add call and apply examples

--- foundation.js
+++ foundation.js
@@ -104,3 +104,22 @@ const obj = {
 };
 
 obj.sing();
+
+// Call, Apply
+
+const wizard = {
+  name: "Merlin",
+  health: 50,
+  heal(num1, num2) {
+    return (this.health += num1 + num2);
+  },
+};
+
+const archer = {
+  name: "Robin Hood",
+  health: 30,
+};
+
+console.log("1", archer);
+wizard.heal.apply(archer, [100, 30]);
+console.log("2", archer);
